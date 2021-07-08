2020c86ffb74d1845518ebf00744b61b2a314d9b
KaOSrp19 <kevinramos28101999@hotmail.com>
2021-07-08 17:20:13 -0400

    Configurando nuestro tema material

---
+++ 2/KurtzPel/app/src/main/java/com/kaosrp19/kurtzpel/formulario.java
@@ -0,0 +1,14 @@
+package com.kaosrp19.kurtzpel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class formulario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario);
+    }
+}
